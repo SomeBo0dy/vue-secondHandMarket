1d4be8ed414e0ed82f86a0480729690c93b3e843
浪里小白虫… <xyj18105857856@163.com>
2024-01-06 16:23:18 +0800

    完成登录表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+//表单对象
+const form = ref({
+  account: "",
+  password: "",
+});
+
+//规则对象
+const rules = {
+  account: [{ required: true, message: "账号不得为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不得为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码长度为6-14个字符", trigger: "blur" },
+  ],
+};
+</script>
 
 <template>
   <div>
@@ -21,12 +38,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :model="form"
+              :rules="rules"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
